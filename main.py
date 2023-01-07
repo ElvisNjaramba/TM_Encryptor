a42f29c5659cbc3a6cf3471afc7467be9db4af27
Tariq Marcel <kigutanjaramba@gmail.com>
2023-01-07 04:35:32 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,50 +1,81 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as Tk
 import tkinter.filedialog as fd
+from PIL import ImageTk
 import os
 from cryptography.fernet import Fernet
+import os
+import speech_recognition as sr
+import pyttsx3
 
-# Create an instance of tkinter frame or window
-win = Tk()
+win= Tk.Tk()
+win.geometry("650x300")
+win.title("Tm_encryptor")
+r = sr.Recognizer()
 
-# Set the geometry of tkinter frame
-win.geometry("700x350")
-win.title("@Tariq Marcel")
-bgimg= PhotoImage(file = "hacker-25938.png")
-win.iconphoto(True, bgimg)
-win.configure(bg='black')
+lbl = Tk.Label(win, text='welcome to Tmspace').pack()
 
-def open_file():
-    global file1
-    file1 = fd.askopenfilenames(parent=win, title='Choose a File')
-    print(win.splitlist(file1))
 
-# Add a Label widget
-label = Label(win, text="This is an encryption decryption program which one can use to encrypt any type of file.", font=('Aerial 11'))
-label.pack(pady=30)
-label1 =Label(win, text=" One can only decrypt the files with the key generated and also the pass phrase set.", pady=5).pack()
-# Add a Button Widget
-ttk.Button(win, text="Select a File", command=open_file).pack()
+def openNewWindow():
+    new= Tk.Toplevel(win)
+    new.geometry("650x300")
+    new.title("@TM Encryptor")
+    new.configure(bg="black")
 
-def encryptor():
-    key = Fernet.generate_key()
-    with open("thekey.key", "wb") as thekey:
-        thekey.write(key)
-        for file in file1:
-            with open(file, "rb") as thefile:
-                contents = thefile.read()
-            contents_encrypted = Fernet(key).encrypt(contents)
-            with open(file, "wb") as thefile:
-                thefile.write(contents_encrypted)
+    new.wm_transient(win)
 
-    print("YOUR FILES HAVE BEEN ENCRYPTED")
 
-ttk.Button(win, text="Encrypt", command=encryptor).pack()
+    def open_file():
+        global file1
+        file1 = fd.askopenfilenames(parent=new, title='Choose a File')
+        label1 = Tk.Label(new, text=file1).pack(pady=20)
 
-def decryptor():
-    secret_phrase = "Tariq"
-    user_phrase = input("ENTER KEY TO DECRYPT: ")
-    if secret_phrase == user_phrase:
+
+    menubar = Tk.Menu(new)
+    new.config(menu=menubar)
+    file_menu = Tk.Menu(menubar)
+
+    # add a menu item to the menu
+    file_menu.add_command(
+        label='open file',
+        command=open_file
+    )
+
+    file_menu.add_command(
+        label='Exit',
+        command=win.destroy
+    )
+
+    # add the File menu to the menubar
+    menubar.add_cascade(
+        label="Menu",
+        menu=file_menu
+    )
+
+    # Add a Label widget
+    label = Tk.Label(new, text="This is an encryption decryption program which one can use to encrypt any type of file.",
+                  font=('Aerial 11')).pack(pady=30)
+
+    def encryptor():
+        try:
+            key = Fernet.generate_key()
+            with open("thekey.key", "wb") as thekey:
+                thekey.write(key)
+            for file in file1:
+                with open(file, "rb") as thefile:
+                    contents = thefile.read()
+                contents_encrypted = Fernet(key).encrypt(contents)
+                with open(file, "wb") as thefile:
+                    thefile.write(contents_encrypted)
+            label2 = Tk.Label(new,
+                           text="Your files have been encrypted!!").pack()
+
+        except:
+            label9 = Tk.Label(new, text="An Error Occured.").pack()
+
+    Tk.Button(new, text="Encrypt", command=encryptor).pack()
+
+    def decryptor():
+        try:
             with open("thekey.key", "rb") as key:
                 secretkey = key.read()
             for file in file1:
@@ -54,11 +85,17 @@ def decryptor():
                 with open(file, "wb") as thefile:
                     thefile.write(contents_decrypted)
 
-            print("YOUR FILES HAVE BEEN DECRYPTED")
-ttk.Button(win, text="Decrypt", command=decryptor).pack()
+            label3 = Tk.Label(new, text="YOUR FILES HAVE BEEN DECRYPTED").pack()
 
+        except:
+            label4 = Tk.Label(new, text="An error occured").pack()
 
-exit_button = Button(win, text="Exit", command=win.destroy)
-exit_button.pack(pady=20)
+    quit = Tk.Button(new, text="Decrypt", command=decryptor).pack()
+
+def open():
+    openNewWindow()
+
+submit1= Tk.Button(win, text="open", command=open).pack()
 
 win.mainloop()
+
